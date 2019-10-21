c86f34317ad0f1733a4f2715722c60f38e1e120a
Vagrant Default User <vagrant@control>
2019-10-21 10:12:01 +0000

    repare

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from flask import Flask, escape, request
+from flask import Flask
 
 APP = Flask(__name__)
 
 @APP.route('/')
 def hello():
-#     
+# commentaire à ajouter     
     return "Hello world!"
-
 if __name__ == '__main__':
     APP.run(host="0.0.0.0")
-
-
