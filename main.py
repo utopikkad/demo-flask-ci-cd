889ca03ecf2d2314d213c3f367d02bb54e65ee01
Vagrant Default User <vagrant@control>
2019-10-21 10:55:57 +0000

    rate 10 for my code

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+""" fichier main test """
 from flask import Flask
 
 APP = Flask(__name__)
 
 @APP.route('/')
 def hello():
+    """ doc """
 # commentaire à ajouter
     return "Hello world!"
+# Listen
 if __name__ == '__main__':
     APP.run(host="0.0.0.0")
